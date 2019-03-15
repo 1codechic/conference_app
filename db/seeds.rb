4cc460fb97a13d029e7dbe0c6ae4cbaaedddd05c
1codechic <1codechic@devchics.com>
2019-03-15 19:09:00 -0400

    added Meeting model, seeded Meeting database with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'ffaker'
+# require 'ffaker'
 
-25.times do 
-  Speaker.create([
-    first_name: FFaker::Name.first_name,
-    last_name: FFaker::Name.last_name,
-    email: FFaker::Internet.email
-  ]) 
-end
+# 25.times do 
+#   Speaker.create([
+#     first_name: FFaker::Name.first_name,
+#     last_name: FFaker::Name.last_name,
+#     email: FFaker::Internet.email
+#   ]) 
+# end
+
+meeting4 = Meeting.new(title: "Play Date with Kendall", agenda: "Run around with Kendall in the park", time: "11:15am")
+meeting4.save
+
+meeting5 = Meeting.new(title: "Rails Migrations", agenda: "Discuss rails migrations", time: "2:15pm")
+meeting5.save
+
+meeting6 = Meeting.new(title: "To code or not to code", agenda: "Why or Why not", time: "3:15pm")
+meeting6.save
+
+
